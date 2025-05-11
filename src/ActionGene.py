771b5247274273cc9f378a7403117f070855306a
JohnAsaro <johnnyasaro63@gmail.com>
2025-05-11 04:12:30 -0400

    turning made a bit more realistic

--- src/ActionGene.py
+++ src/ActionGene.py
@@ -22,7 +22,7 @@ class ActionGene:
         self.thrust = 0                                   # Thrust starts at 0
         if gene[2] <= turn_roll and gene[2] > 0 and self.agent.agent_data["speed"] < 10:          # If we have a chance of thrusting this frame, thrust if we roll lower or equal to our chance, speed limit 10
             self.thrust = 1     
-        self.turn_quantity = int((gene[3] + 0) * 3)     # Scaling factor for turn amount, turn amount doesnt directly correspond to degrees
+        self.turn_quantity = int((gene[3] + 0) * 1)     # Scaling factor for turn amount, turn amount doesnt directly correspond to degrees
         self.turn_target = gene[4]                        # Target to turn towards (0-7 encoded values)
         self.action_priority = gene[5]                       # 0 if we prioritize thrusting over turning on this action, else 1
         # Execute the actions immediately upon gene creation
